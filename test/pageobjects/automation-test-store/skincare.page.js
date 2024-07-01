82373dfa4c14565c8e09117dd095bbbfc2fe0f39
Fabian Limon <fabian.limon.rodriguez@gmail.com>
2024-07-01 14:45:26 -0600

    page object modelling added to add-items-to-basket.spec.js test

---
+++ test/pageobjects/automation-test-store/skincare.page.js
@@ -0,0 +1,68 @@
+import BasePage from "./base.page";
+import ItemComponent from "../automation-test-store/components/item.comp"
+
+class SkinCarePage extends BasePage{
+    get itemComponent(){
+        return ItemComponent;
+    }
+
+    async addSpecificItems_ValidateTotal(item1, item2){
+        const skincareProducts_Header_Links = await ItemComponent.itemHeaderLinks;
+
+        const itemPrices = [];
+
+        for (const header of skincareProducts_Header_Links) {
+
+            const tempHeaderText = await header.getText();
+                        
+            if(tempHeaderText.toLowerCase() == item1.toLowerCase() ||
+            tempHeaderText.toLowerCase() == item2.toLowerCase()){
+
+                const attr = await header.getAttribute("href");
+                //console.log(attr);
+                const itemId = attr.split("id=").pop(); // 93 66
+                //console.log(itemId);
+
+                // //a[@data-id="93"]
+                await $('//a[@data-id="' + itemId + '"]').click();
+
+                // //a[@data-id="93"]/following-sibling::div/div[@class='pricenew']|//a[@data-id="66"]/following-sibling::div/div[@class='oneprice']
+                itemPrices.push(
+                    await $("//a[@data-id='" + itemId + "']/following-sibling::div/div[@class='pricenew']"
+                        + "|//a[@data-id='" + itemId + "']/following-sibling::div/div[@class='oneprice']").getText()
+                )
+
+            }
+            
+        }
+
+        const formattedItemPrices = [];
+
+        itemPrices.forEach ((price) => {
+
+            formattedItemPrices.push(price.replace("$",""));
+
+        });
+
+        var itemsTotal = 0;
+        formattedItemPrices.forEach(price => itemsTotal += parseFloat(price));
+        console.log("Items total: " + itemsTotal);
+
+        await $("//span[text()='Cart']").click();
+        await expect(browser).toHaveUrlContaining("checkout");
+
+        // //span[text()='Flat Shipping Rate:']/../following-sibling::td
+        var tempShippingRate = await $("//span[text()='Flat Shipping Rate:']/../following-sibling::td").getText();
+        var shippingRate = tempShippingRate.replace('$','');
+
+        itemsTotal = itemsTotal + parseFloat(shippingRate);
+        console.log("Items total + shipping rate = " + itemsTotal);
+
+        // extract cart total
+        var cartTotal = await $("//span[text()='Total:']/../following-sibling::td").getText();
+        cartTotal = cartTotal.replace('$','');
+
+        expect(itemsTotal).toEqual(parseFloat(cartTotal));
+    }
+}
+export default new SkinCarePage();
--- test/specs/automation-test-store/add-items-to-basket.spec.js
+++ test/specs/automation-test-store/add-items-to-basket.spec.js
@@ -1,74 +1,29 @@
-import homePage from "../../pageobjects/automation-test-store/home.page";
+import HomePage from "../../pageobjects/automation-test-store/home.page";
+import SkinCarePage from "../../pageobjects/automation-test-store/skincare.page";
 
 describe('Add items to basket', () => {
 
     beforeEach(async () => {
         
         //await browser.url("https://automationteststore.com/");
-        await homePage.open();
+        await HomePage.open();
     });
 
     it("Add specific 'skincare produts' to basket and validate car total", async() => {
 
-        const skincareLinks = await $$("//a[contains(text(), 'Skincare')]");
-        const itemPrices = [];
-        await skincareLinks[1].click();
+        //const skincareLinks = await $$("//a[contains(text(), 'Skincare')]");
+        //await skincareLinks[1].click();
+        await HomePage.categoryMenuComponent.categoryMenuLink('Skincare')[1].click();
 
-        const skincareProducts_Header_Links = await $$('.fixed_wrapper .prdocutname');
-        for (const header of skincareProducts_Header_Links) {
+        //const skincareProducts_Header_Links = await $$('.fixed_wrapper .prdocutname');
+        const skincareProducts_Header_Links = await SkinCarePage.itemComponent.itemHeaderLinks;
 
-            const tempHeaderText = await header.getText();
-                        
-            if(tempHeaderText.toLowerCase() == "creme precieuse nuit 50ml" ||
-            tempHeaderText.toLowerCase() == "total moisture facial cream"){
+        await SkinCarePage.addSpecificItems_ValidateTotal("creme precieuse nuit 50ml","total moisture facial cream");
 
-                const attr = await header.getAttribute("href");
-                //console.log(attr);
-                const itemId = attr.split("id=").pop(); // 93 66
-                //console.log(itemId);
+        //creme precieuse nuit 50ml
+        //total moisture facial cream
 
-                // //a[@data-id="93"]
-                await $('//a[@data-id="' + itemId + '"]').click();
-
-                // //a[@data-id="93"]/following-sibling::div/div[@class='pricenew']|//a[@data-id="66"]/following-sibling::div/div[@class='oneprice']
-                itemPrices.push(
-                    await $("//a[@data-id='" + itemId + "']/following-sibling::div/div[@class='pricenew']"
-                        + "|//a[@data-id='" + itemId + "']/following-sibling::div/div[@class='oneprice']").getText()
-                )
-
-            }
-            
-        }
-
-        const formattedItemPrices = [];
-
-        itemPrices.forEach ((price) => {
-
-            formattedItemPrices.push(price.replace("$",""));
-
-        });
-
-        var itemsTotal = 0;
-        formattedItemPrices.forEach(price => itemsTotal += parseFloat(price));
-        console.log("Items total: " + itemsTotal);
-
-        await $("//span[text()='Cart']").click();
-        await expect(browser).toHaveUrlContaining("checkout");
-
-        // //span[text()='Flat Shipping Rate:']/../following-sibling::td
-        var tempShippingRate = await $("//span[text()='Flat Shipping Rate:']/../following-sibling::td").getText();
-        var shippingRate = tempShippingRate.replace('$','');
-
-        itemsTotal = itemsTotal + parseFloat(shippingRate);
-        console.log("Items total + shipping rate = " + itemsTotal);
-
-        // extract cart total
-        var cartTotal = await $("//span[text()='Total:']/../following-sibling::td").getText();
-        cartTotal = cartTotal.replace('$','');
-
-        expect(itemsTotal).toEqual(parseFloat(cartTotal));
-
-        await browser.pause(3000);
+        //await browser.pause(3000);
 
         
     });
